131acf7f3a8961e0d8a2c9351bf2219c15c574f1
developerfahad1 <fahadgraphic.pk>
2024-05-07 14:55:29 +0500

    app.js and index.html file created

---
+++ app.js
@@ -0,0 +1,8 @@
+// const h1 = document.getElementById('head');
+// const h1 = document.getElementsByClassName('head');
+const h1 = document.querySelector('head');
+
+console.log(head);
+
+h1.innerHTML = 'changed through by js'
+console.log('change through by js');
